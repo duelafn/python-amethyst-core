d51f5ecebc6d6c5c29d1d90249e771d41a647603
Dean Serenevy <dean@serenevy.net>
2017-04-04 12:40:00 -0400

    Test more attributes of Attr(): convert, verify, isa, default, builder

--- tests/test_attr.py
+++ tests/test_attr.py
@@ -8,6 +8,14 @@ import six
 
 from amethyst.core import Object, Attr
 
+class Exception_Eager(Exception): pass
+class Counter(object):
+    def __init__(self):
+        self.value = 0
+    def step(self):
+        self.value += 1
+        return self.value
+
 class MyTest(unittest.TestCase):
 
     def test_basic(self):
@@ -15,7 +23,6 @@ class MyTest(unittest.TestCase):
             amethyst_register_type = False
             foo = Attr(int)
             bar = Attr(isa=six.text_type).strip()
-            baz = Attr(isa=(int, list))
 
         myobj = MyObject(dict(foo=23, bar=" Hello "))
 
@@ -25,13 +32,78 @@ class MyTest(unittest.TestCase):
         myobj.set("foo", 15, foo=5)
         self.assertEqual(myobj.foo, 15, ".set() takes positional or kwargs and prefers positional")
 
-        # Explicitly support list/tuple of isa types
-        myobj.set(baz=23)
-        self.assertEqual(myobj.baz, 23, "isa tuple:int")
-        myobj.set(baz=["foo", "bar"])
-        self.assertEqual(myobj.baz, ["foo", "bar"], "isa tuple:list")
+    def test_Attr_unit(self):
+        counter = Counter()
+        def boom_eager():
+            raise Exception_Eager("boom!")
+
+        class MyObject(Object):
+            amethyst_verifyclass = False
+            amethyst_register_type = False
+            a = Attr(int)
+            b = Attr(float)
+            c = Attr(verify=(lambda x: 0 < x < 10))
+            d = Attr(isa=int)
+            e = Attr(isa=(int, list))
+            f = Attr(default=42)
+            g = Attr(default=(lambda: 22))
+            h = Attr(default=(lambda: counter.step()))
+            i = Attr(builder=(lambda: self.assertFalse(True, "builder called lazily")))
+            j = Attr(builder=(lambda: 12))
+
+        class MyEagerTest(Object):
+            amethyst_verifyclass = False
+            amethyst_register_type = False
+            k = Attr(default=(lambda: boom_eager()))
+
+        myobj = MyObject()
+
+        self.assertIsNone(myobj.a, "unset is none")
+
+        myobj.set("a", "23")
+        self.assertEqual(myobj.a, 23, "Attr(int)")
         with self.assertRaises(ValueError):
-            myobj.set("foo", "Not an int or list")
+            myobj.set("a", "34.2")
+        with self.assertRaises(ValueError):
+            myobj.set("a", "Not an int")
+
+        myobj.set("b", "23.3")
+        self.assertEqual(myobj.b, 23.3, "Attr(float)")
+
+        myobj.set("c", 3.3)
+        self.assertEqual(myobj.c, 3.3, "verify 0 < x < 10")
+        with self.assertRaises(ValueError):
+            myobj.set("c", 12)
+
+        myobj.set("d", 3)
+        self.assertEqual(myobj.d, 3, "isa int")
+        with self.assertRaises(ValueError):
+            myobj.set("c", 12)
+
+        myobj.set(e=23)
+        self.assertEqual(myobj.e, 23, "isa tuple:int")
+        myobj.set(e=["foo", "bar"])
+        self.assertEqual(myobj.e, ["foo", "bar"], "isa tuple:list")
+        with self.assertRaises(ValueError):
+            myobj.set("e", "Not an int or list")
+
+        self.assertEqual(myobj.f, 42, "default")
+        self.assertEqual(myobj.g, 22, "default callable")
+
+        self.assertEqual(myobj.h, 1, "default callable caches (first)")
+        self.assertEqual(myobj.h, 1, "default callable caches (second)")
+
+        myobj2 = MyObject()
+        myobj3 = MyObject()
+        self.assertEqual(myobj2.h, 2, "default callable distinct each object (2)")
+        self.assertEqual(myobj3.h, 3, "default callable distinct each object (3)")
+
+        # DO NOT get .i, need to test builder is lazy
+        # self.assertEqual(myobj.i, 12, "builder")
+        self.assertEqual(myobj.j, 12, "builder")
+
+        with self.assertRaises(Exception_Eager):
+            myobj = MyEagerTest()
 
     def test_README_validation(self):
         class MyObject(Object):
