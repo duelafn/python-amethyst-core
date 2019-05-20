2dc422c64d6abcd2fd8a5f718b2dd0cbf566ae26
Dean Serenevy <dean@serenevy.net>
2019-05-20 09:12:45 -0400

    amethyst-core v0.7.1

--- amethyst/core/__init__.py
+++ amethyst/core/__init__.py
@@ -4,7 +4,7 @@ A sober python base library
 """
 # SPDX-License-Identifier: LGPL-3.0
 
-__version__ = "0.7.0"
+__version__ = "0.7.1"
 
 from .util import *   # noqa: F403, F401
 from .obj  import *   # noqa: F403, F401
